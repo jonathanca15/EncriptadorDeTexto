77edf8f7b58011f6fe98fdbf287ba85619b5fe72
jonathanca15 <156934741+jonathanca15@users.noreply.github.com>
2024-04-04 09:41:59 -0600

    Delete Encriptador directory

--- Encriptador/encript.js
+++
@@ -1,5 +0,0 @@
-function encryptText() {
-    var inputText = document.getElementById("inputText").value;
-    var encryptedText = btoa(inputText); // Encoding text to base64
-    document.getElementById("encryptedText").value = encryptedText;
-}
